ed5c08a8ce939919024fd1545fd6413c0cdac8c0
Al Davidson <apdavidson@gmail.com>
2018-06-19 15:54:43 +0100

    add parallel status checks

--- Gemfile
+++ Gemfile
@@ -49,6 +49,9 @@ gem 'pundit'
 gem 'rails', '~> 5.2.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+
+# parallel HTTP requests
+gem 'typhoeus'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 # Use Uglifier as compressor for JavaScript assets
--- app/services/status_service.rb
+++ app/services/status_service.rb
@@ -22,17 +22,54 @@ class StatusService
 
   # TODO: implement properly when we have services running
   def self.last_status(service:, environment:)
-    {
-      status: nil,
-      timestamp: nil
-    }
+    nil
   end
 
   def self.check(service:, environment:)
-    r = Net::HTTP.get_response(URI.parse(environment.url_for(service)))
+    url = environment.url_for(service)
+    start = Time.now
+
+    response = begin
+      r = Net::HTTP.get(URI.parse(url))
+    rescue SocketError => e
+      nil
+    end
+    time_taken = Time.now - start
+
     {
-      status: r.code,
-      timestamp: Time.now
+      environment: environment.slug,
+      service: service.slug,
+      status: response.try(:code),
+      timestamp: Time.now,
+      time_taken: time_taken,
+      url: url
     }
+  end
+
+  def self.check_in_parallel( service:,
+                              environments: ServiceEnvironment.all_keys,
+                              timeout: 5)
+    hydra = Typhoeus::Hydra.new
+    requests = environments.map do |env|
+      env = ServiceEnvironment.find(env)
+      req = Typhoeus::Request.new(env.url_for(service), timeout: timeout)
+      hydra.queue(req)
+      req
+    end
+    # blocks until all completed
+    hydra.run
+    index = 0
+    checks = requests.map do |request|
+      resp = request.response
+      code = resp.response_code == 0 ? nil : resp.response_code
+      {
+        environment: environments[index],
+        service: service.slug,
+        status: code,
+        timestamp: Time.now,
+        time_taken: resp.total_time,
+        url: request.url
+      }
+    end
   end
 end
