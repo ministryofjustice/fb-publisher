6926508d5ac9b6ee8bfe6f42eea74bcb9cee7dfb
Al Davidson <apdavidson@gmail.com>
2018-07-05 13:36:59 +0100

    use VersionControlService abstraction

--- app/jobs/deploy_service_job.rb
+++ app/jobs/deploy_service_job.rb
@@ -7,28 +7,34 @@ class DeployServiceJob < ApplicationJob
     # Check out the code
     json_dir = File.join(temp_dir, 'json')
     config_dir = File.join(temp_dir, 'config')
-    GitService.clone_repo(repo_url: deployment.service.git_repo_url, to_dir: json_dir)
-    GitService.checkout(ref: deployment.commit_sha, dir: json_dir)
-    deployment.commit_sha = GitService.current_commit_sha(dir: json_dir, short: true)
 
+    deployment.commit_sha = VersionControlService.checkout(
+      repo_url: deployment.service.git_repo_url,
+      ref: deployment.commit_sha,
+      to_dir: json_dir
+    )
 
     # if the json we want is not in the root of the repo,
     # from now on we need to work in that sub dir
     json_dir = File.join(json_dir, json_sub_dir) if json_sub_dir.present?
 
-    built_service = DeploymentService.build(
+    built_service_tag = DeploymentService.build(
       environment_slug: deployment.environment_slug,
       service: deployment.service,
       json_dir: json_dir
     )
     DeploymentService.push(
-      image: built_service[:tag],
+      image: built_service_tag,
       environment_slug: deployment.environment_slug
     )
+
+    # TODO: reorganize this so that you can configure everything
+    # BEFORE restarting (currently the configure call fails if the
+    # service doesn't exist within Kubernetes)
     DeploymentService.restart(
       environment_slug: deployment.environment_slug,
       service: deployment.service,
-      tag: built_service[:tag]
+      tag: built_service_tag
     )
 
     DeploymentService.configure(
