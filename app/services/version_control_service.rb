68d9e74f55982c920ecc19d8065122dd85ec334e
Al Davidson <apdavidson@gmail.com>
2018-07-12 10:30:17 +0100

    GitAdapter & VersionControlService specs

--- app/services/version_control_service.rb
+++ app/services/version_control_service.rb
@@ -1,7 +1,7 @@
 class VersionControlService
   # Clones repo, checks out the given ref to the given dir,
   # and returns the current commit id of that ref
-  def self.checkout(repo_url:, ref:, to_dir: Dir.mktmpdir)
+  def self.checkout(repo_url:, ref: nil, to_dir: tmpdir)
     GitAdapter.clone_repo(repo_url: repo_url, to_dir: to_dir)
     GitAdapter.checkout(ref: ref, dir: to_dir)
     current_commit(dir: to_dir)
@@ -9,5 +9,11 @@ class VersionControlService
 
   def self.current_commit(dir:)
     GitAdapter.current_commit_sha(dir: dir, short: true)
+  end
+
+  private
+
+  def self.tmpdir
+    Dir.mktmpdir
   end
 end
---
+++ spec/services/version_control_service_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+describe VersionControlService do
+  describe '.current_commit' do
+    it 'gets the short commit SHA of the given directory' do
+      expect(GitAdapter).to receive(:current_commit_sha).with(dir: '/my/dir', short: true).and_return('123456')
+      described_class.current_commit(dir: '/my/dir')
+    end
+
+    it 'returns the short commit SHA of the given directory' do
+      allow(GitAdapter).to receive(:current_commit_sha).with(dir: '/my/dir', short: true).and_return('123456')
+      expect(described_class.current_commit(dir: '/my/dir')).to eq('123456')
+    end
+  end
+
+  describe '.checkout' do
+    before do
+      allow(GitAdapter).to receive(:clone_repo)
+      allow(GitAdapter).to receive(:checkout)
+      allow(described_class).to receive(:current_commit).and_return 'my current commit'
+    end
+
+    context 'given a to_dir' do
+      it 'clones the given repo to the given dir' do
+        expect(GitAdapter).to receive(:clone_repo).with(repo_url: anything, to_dir: '/my/dir')
+        described_class.checkout(repo_url: 'myrepourl', to_dir: '/my/dir')
+      end
+
+      it 'checks out the given ref in the given dir' do
+        expect(GitAdapter).to receive(:checkout).with(ref: 'myref', dir: '/my/dir')
+        described_class.checkout(repo_url: 'myrepourl', ref: 'myref', to_dir: '/my/dir')
+      end
+
+      it 'gets the current commit in the given dir' do
+        expect(described_class).to receive(:current_commit).with(dir: '/my/dir').and_return 'my current commit'
+        described_class.checkout(repo_url: 'myrepourl', to_dir: '/my/dir')
+      end
+    end
+
+    context 'given no to_dir' do
+      before do
+        allow(described_class).to receive(:tmpdir).and_return('/tmp/dir')
+      end
+
+      it 'clones the given repo to a tmpdir' do
+        expect(GitAdapter).to receive(:clone_repo).with(repo_url: 'myrepourl', to_dir: '/tmp/dir')
+        described_class.checkout(repo_url: 'myrepourl')
+      end
+
+      it 'checks out the given ref in the tmpdir' do
+        expect(GitAdapter).to receive(:checkout).with(ref: 'myref', dir: '/tmp/dir')
+        described_class.checkout(repo_url: 'myrepourl', ref: 'myref')
+      end
+
+      it 'gets the current commit in the tmpdir' do
+        expect(described_class).to receive(:current_commit).with(dir: '/tmp/dir').and_return 'my current commit'
+        described_class.checkout(repo_url: 'myrepourl')
+      end
+    end
+
+    it 'returns the current commit' do
+      expect(described_class.checkout(repo_url: 'myrepourl')).to eq('my current commit')
+    end
+  end
+end
