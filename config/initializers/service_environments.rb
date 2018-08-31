2df9bd05c5b38e4d5ffe08b0609a585ef9d7c0d9
Al Davidson <apdavidson@gmail.com>
2018-08-31 11:48:55 +0100

    make kubectl_context an env var to allow it to be empty on prod

--- config/initializers/service_environments.rb
+++ config/initializers/service_environments.rb
@@ -1,7 +1,7 @@
 ALL_ENVS = {
   dev: {
     deployment_adapter: 'cloud_platform',
-    kubectl_context: 'cp-live',
+    kubectl_context: ENV['KUBECTL_CONTEXT'],
     name: 'Development',
     namespace: 'formbuilder-services-dev',
     protocol: 'https://',
@@ -9,7 +9,7 @@ ALL_ENVS = {
   },
   staging: {
     deployment_adapter: 'cloud_platform',
-    kubectl_context: 'cp-live',
+    kubectl_context: ENV['KUBECTL_CONTEXT'],
     name: 'Staging',
     namespace: 'formbuilder-services-staging',
     protocol: 'https://',
@@ -17,7 +17,7 @@ ALL_ENVS = {
   },
   production: {
     deployment_adapter: 'cloud_platform',
-    kubectl_context: 'cp-live',
+    kubectl_context: ENV['KUBECTL_CONTEXT'],
     name: 'Production',
     namespace: 'formbuilder-services-production',
     protocol: 'https://',
