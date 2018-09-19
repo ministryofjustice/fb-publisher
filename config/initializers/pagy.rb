cc73d3a24a4a35d7a7c83d313e50e670efd516a6
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2018-09-19 22:32:48 +0100

    Display pagination for services
    The pagination allows up to 10 services to be displayed on a page.
    'Next' and 'Prev' links allow the user to navigate through the pages.

---
+++ config/initializers/pagy.rb
@@ -0,0 +1,19 @@
+# See the Pagy documentation: https://ddnexus.github.io/pagy/extras/array
+class Pagy
+  # Add specialized backend methods to paginate array collections
+  module Backend
+    private
+
+    # Return Pagy object and items
+    def pagy_array(array, vars={})
+      pagy = Pagy.new(pagy_array_get_vars(array, vars))
+      return pagy, array[pagy.offset, pagy.items]
+    end
+
+    def pagy_array_get_vars(array, vars)
+      # Return the merged variables to initialize the Pagy object
+      { count: array.count,
+        page:  params[vars[:page_param]||VARS[:page_param]] }.merge!(vars)
+    end
+  end
+end
