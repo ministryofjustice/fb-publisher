f13d30cd5ed9b4d47bebebf0e90a0c1a7b491914
Al Davidson <apdavidson@gmail.com>
2018-10-08 16:12:25 +0100

    fix job_log and Resque redis for elasticache over TLS

--- config/initializers/job_log.rb
+++ config/initializers/job_log.rb
@@ -2,8 +2,9 @@ url = ENV["REDISCLOUD_URL"] || ENV['REDIS_URL']
 job_log_adapter_class = FileLogAdapter
 if url.present?
   begin
-    uri = URI.parse(url)
-    Rails.configuration.x.job_log_redis = Redis.new(host:uri.host, port:uri.port, password:uri.password)
+    uri_with_protocol = (ENV['REDIS_PROTOCOL'] || 'redis://') + url
+    uri = URI.parse(uri_with_protocol)
+    Rails.configuration.x.job_log_redis = Redis.new(url: uri_with_protocol, password:uri.password)
     job_log_adapter_class = RedisLogAdapter
   rescue URI::InvalidURIError
     puts "could not parse a valid Redis URI from #{url} - falling back to file log"
