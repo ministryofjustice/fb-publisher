a395caa992bd42f307d8254b560eb7b867587ee5
Al Davidson <apdavidson@gmail.com>
2018-10-09 11:17:37 +0100

    correct password reference

--- config/initializers/job_log.rb
+++ config/initializers/job_log.rb
@@ -4,7 +4,10 @@ if url.present?
   begin
     uri_with_protocol = (ENV['REDIS_PROTOCOL'] || 'redis://') + url.to_s
     uri = URI.parse(uri_with_protocol)
-    Rails.configuration.x.job_log_redis = Redis.new(url: uri_with_protocol, password:uri.password)
+    Rails.configuration.x.job_log_redis = Redis.new(
+      url: uri_with_protocol,
+      password: ENV['REDIS_AUTH_TOKEN']
+    )
     job_log_adapter_class = RedisLogAdapter
   rescue URI::InvalidURIError
     puts "could not parse a valid Redis URI from #{url} - falling back to file log"
