2a97e5d9ed6c2d809d8f97194a988054c238ec17
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2018-10-16 12:45:41 +0100

    Display dates/times in user's timezone
    Create an user edit page where user can change their
    default timezone ('London') to one of 149 different
    timezones. This change displays the frontent dates/times
    into the user's chosen timezone.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+require 'byebug'
+class UsersController < ApplicationController
+  before_action :require_user!
+
+  def edit
+    @timezones = ActiveSupport::TimeZone.all.collect(&:name).sort.map! { |zone| [zone, zone] }
+  end
+
+  def update
+    if @current_user.update_attributes(user_params)
+      @current_user.save!
+      flash[:success] = I18n.t('users.update.success')
+    else
+      flash[:error] = I18n.t('users.update.error')
+    end
+    redirect_to action: :edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :timezone, :updated_at)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_144902) do
+ActiveRecord::Schema.define(version: 2018_10_16_120821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2018_10_03_144902) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "timezone", default: "London"
   end
 
   add_foreign_key "identities", "users"
