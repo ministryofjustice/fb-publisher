f2bd422405c85fa704cc4c3a3316e86dfb77900b
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2018-12-07 17:30:15 +0000

    Amended code to make sure that we were not already adding someone who is already a member of the super_admin team. Refactored to use the new way of finding records

--- db/data_migrations/20181207103953_add_users_to_super_admin_team.rb
+++ db/data_migrations/20181207103953_add_users_to_super_admin_team.rb
@@ -1,21 +1,21 @@
 class AddUsersToSuperAdminTeam < ActiveRecord::DataMigration
   def up
-    team = Team.find_by_name('Super Admin')
+    team = Team.find_by(name: 'Super Admin')
 
     return if team.nil?
 
     users = []
-    users << User.find_by_email('andrien.ricketts@digital.justice.gov.uk')
-    users << User.find_by_email('alex.robinson@digital.justice.gov.uk')
-    users << User.find_by_email('sophie.osman@digital.justice.gov.uk')
-    users << User.find_by_email('anna.brennan@digital.justice.gov.uk')
-    users << User.find_by_email('daphne.haitas@digital.justice.gov.uk')
-    users << User.find_by_email('elliot.bouher@digital.justice.gov.uk')
+    users << User.find_by(email: 'andrien.ricketts@digital.justice.gov.uk')
+    users << User.find_by(email: 'alex.robinson@digital.justice.gov.uk')
+    users << User.find_by(email: 'sophie.osman@digital.justice.gov.uk')
+    users << User.find_by(email: 'anna.brennan@digital.justice.gov.uk')
+    users << User.find_by(email: 'daphne.haitas@digital.justice.gov.uk')
+    users << User.find_by(email: 'elliot.bouher@digital.justice.gov.uk')
 
     users.each do |user|
       next if user.nil?
 
-      next unless TeamMember.find_by_user_id(user.id).nil?
+      next unless TeamMember.find_by(user_id: user.id, team_id: team.id).nil?
 
       TeamMember.create(user_id: user.id, team_id: team.id, created_by_user_id: team.created_by_user_id)
     end
