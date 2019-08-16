f716f9b22fcfc7f8a3e335262830d846742be40b
Emile <emile.swarts123@gmail.com>
2019-08-16 15:30:56 +0100

    Dynamic bundle flags based on environment
    To only install the correct dependencies per environment, we need to set a flag
    which differs from CI deployments to local dev installs

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -9,5 +9,5 @@ for TYPE in web worker
 do
   REPO_NAME=${REPO_SCOPE}/fb-publisher-${TYPE}
   echo "Building ${REPO_NAME}"
-  docker build -f docker/${TYPE}/Dockerfile -t ${REPO_NAME}:${TAG} --build-arg BUNDLE_FLAGS='--without test development' --build-arg BASE_IMAGE=${REPO_SCOPE}/fb-publisher-base:${TAG} .
+  docker build -f docker/${TYPE}/Dockerfile -t ${REPO_NAME}:${TAG} --build-arg BUNDLE_FLAGS='--deployment --without test development' --build-arg BASE_IMAGE=${REPO_SCOPE}/fb-publisher-base:${TAG} .
 done
