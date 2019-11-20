890d4a5d575c85aec5d61698fa6f7b03486b86f4
Tom Taylor <tommotaylor@gmail.com>
2019-11-20 16:59:32 +0000

    Revert "Bump sprockets from 3.7.2 to 4.0.0 (#222)" (#243)
    This reverts commit 7287c523181061743f8f89d493f2f995ac9130d2.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,3 @@
 //= link_tree ../images
-//= link application.js
-//= link application.css
-//= link govuk-template.css
-//= link govuk-template-ie6.css
-//= link govuk-template-ie7.css
-//= link govuk-template-ie8.css
-//= link govuk-template-print.css
-//= link fonts.css
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
