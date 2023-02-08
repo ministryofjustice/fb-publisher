5c730503b6b1aa64667c09132f47299300cf9610
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-02-08 17:47:13 +0000

    Fix to HTTParty

--- spec/support/sessions.rb
+++ spec/support/sessions.rb
@@ -15,9 +15,7 @@ def stub_auth0_userinfo(info)
 end
 
 def stub_cookie_variable!(name, value)
-  cookie_jar = Capybara.current_session.driver.browser.current_session\
-                       .instance_variable_get(:@rack_mock_session)\
-                       .cookie_jar
+  cookie_jar = Capybara.current_session.driver.browser.rack_mock_session.cookie_jar
   cookie_jar[:"stub_#{name.to_s}"] = value
 end
 
